35b222a6eab508ffc9e64ac7cf04dd84960f7940
yspark2 <rotqjfrkffo@gmail.com>
2023-08-15 08:54:55 +0900

    0.1

---
+++ app/src/main/java/com/example/netcha/MainPage.kt
@@ -0,0 +1,18 @@
+package com.example.netcha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+
+//        val iv_profile1 = findViewById<ImageView>(R.id.iv_profile1)
+//        val iv_profile2 = findViewById<ImageView>(R.id.iv_profile2)
+//        val iv_profile3 = findViewById<ImageView>(R.id.iv_profile3)
+//        val iv_profile4 = findViewById<ImageView>(R.id.iv_profile4)
+//        val iv_profile5 = findViewById<ImageView>(R.id.iv_profile5)
+    }
+}
