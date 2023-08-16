fc252c2bea897691daefb24d99928f4ecd189d9a
minji-0420 <pongiii17@gmail.com>
2023-08-16 20:48:52 +0900

    detailpage4

--- app/src/main/java/com/example/netcha/MainPage.kt
+++ app/src/main/java/com/example/netcha/MainPage.kt
@@ -1,13 +1,16 @@
 package com.example.netcha
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainPage : AppCompatActivity() {
 
@@ -15,97 +18,43 @@ class MainPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
-        val ivProfile1: ImageView = findViewById(R.id.iv_profile1)
-        val ivProfile2: ImageView = findViewById(R.id.iv_profile2)
-        val ivProfile3: ImageView = findViewById(R.id.iv_profile3)
-        val ivProfile4: ImageView = findViewById(R.id.iv_profile4)
-        val ivProfile5: ImageView = findViewById(R.id.iv_profile5)
-        val ivMovie1: ImageView = findViewById(R.id.iv_movie1)
-        val ivMovie2: ImageView = findViewById(R.id.iv_movie2)
-        val ivMovie3: ImageView = findViewById(R.id.iv_movie3)
-        val ivMovie4: ImageView = findViewById(R.id.iv_movie4)
-        val ivMovie5: ImageView = findViewById(R.id.iv_movie5)
-        val ivMovie6: ImageView = findViewById(R.id.iv_movie6)
-        val tvMovie1: TextView = findViewById(R.id.tv_movie1)
-        val btnSummary1: Button = findViewById(R.id.btn_summary1)
-        val originalMaxLines = tvMovie1.maxLines
-        val expandedMaxLines = Int.MAX_VALUE
 
-        btnSummary1.setOnClickListener(object : View.OnClickListener {
-            var isExpanded = false
-            override fun onClick(p0: View?) {
-                isExpanded = !isExpanded
-                if (isExpanded) {
-                    tvMovie1.maxLines = expandedMaxLines
-                    btnSummary1.text = "접기"
-                } else {
-                    tvMovie1.maxLines = originalMaxLines
-                    btnSummary1.text = "더보기"
+        val imageViewList = listOf(
+            findViewById<ImageView>(R.id.iv_profile1),
+            findViewById<ImageView>(R.id.iv_profile2),
+            findViewById<ImageView>(R.id.iv_profile3),
+            findViewById<ImageView>(R.id.iv_profile4),
+            findViewById<ImageView>(R.id.iv_profile5),
+            findViewById<ImageView>(R.id.iv_movie1),
+            findViewById<ImageView>(R.id.iv_movie2),
+            findViewById<ImageView>(R.id.iv_movie3),
+            findViewById<ImageView>(R.id.iv_movie4),
+            findViewById<ImageView>(R.id.iv_movie5),
+            findViewById<ImageView>(R.id.iv_movie6),
+        )
+        val imageViewMap = mapOf(
+            R.id.iv_profile1 to ProfilePageHeyrin::class.java,
+            R.id.iv_profile2 to ProfilePageJina::class.java,
+            R.id.iv_profile3 to ProfilePageYongseok::class.java,
+            R.id.iv_profile4 to ProfilePageMinji::class.java,
+            R.id.iv_profile5 to ProfilePageDonghyun::class.java,
+            R.id.iv_movie1 to MovieDetailPage1::class.java,
+            R.id.iv_movie2 to MovieDetailPage2::class.java,
+            R.id.iv_movie3 to MovieDetailPage3::class.java,
+            R.id.iv_movie4 to MovieDetailPage4::class.java,
+            R.id.iv_movie5 to MovieDetailPage5::class.java,
+            R.id.iv_movie6 to MovieDetailPage6::class.java,
+        )
+
+        for(imageView in imageViewList){
+            imageView.setOnClickListener {
+                val targetClass = imageViewMap[it.id]
+                targetClass?.let{
+                    val intent = Intent(this, it)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
                 }
             }
-        })
-
-        ivProfile1.setOnClickListener {
-            val intent = Intent(this, ProfilePageHeyrin::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-        }
-        ivProfile2.setOnClickListener {
-            val intent = Intent(this, ProfilePageJina::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-        }
-        ivProfile3.setOnClickListener {
-            val intent = Intent(this, ProfilePageYongseok::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-        }
-        ivProfile4.setOnClickListener {
-            val intent = Intent(this, ProfilePageMinji::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-        }
-        ivProfile5.setOnClickListener {
-            val intent = Intent(this, ProfilePageDonghyun::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-        }
-
-        ivMovie1.setOnClickListener {
-            val intent = Intent(this, MovieDetailPage1::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-            Toast.makeText(this,"마녀 페이지", Toast.LENGTH_SHORT).show()
-        }
-        ivMovie2.setOnClickListener {
-            val intent = Intent(this, MovieDetailPage2::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-            Toast.makeText(this,"행오버3 페이지", Toast.LENGTH_SHORT).show()
-        }
-        ivMovie3.setOnClickListener {
-            val intent = Intent(this, MovieDetailPage3::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-            Toast.makeText(this,"라라랜드 페이지", Toast.LENGTH_SHORT).show()
-        }
-        ivMovie4.setOnClickListener {
-            val intent = Intent(this, MovieDetailPage4::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-            Toast.makeText(this,"극한직업 페이지", Toast.LENGTH_SHORT).show()
-        }
-        ivMovie5.setOnClickListener {
-            val intent = Intent(this, MovieDetailPage5::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-            Toast.makeText(this,"아저씨 페이지", Toast.LENGTH_SHORT).show()
-        }
-        ivMovie6.setOnClickListener {
-            val intent = Intent(this, MovieDetailPage6::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit)
-            Toast.makeText(this,"테이큰 페이지", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/netcha/MovieDetailPage4.kt
+++ app/src/main/java/com/example/netcha/MovieDetailPage4.kt
@@ -1,11 +1,45 @@
 package com.example.netcha
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class MovieDetailPage4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail_page4)
+
+        val movieUrl = findViewById<ImageView>(R.id.url)
+        val viewMore = findViewById<TextView>(R.id.view_more)
+        val movieStory = findViewById<TextView>(R.id.movie_story_detail)
+        val originalMaxLines = movieStory.maxLines
+        val expandedMaxLines = Int.MAX_VALUE
+
+
+        movieUrl.setOnClickListener {
+            var intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://tv.naver.com/v/4747256"))
+            startActivity(intent)
+        }
+
+        viewMore.setOnClickListener(object : View.OnClickListener {
+            var isExpanded = false
+            override fun onClick(p0: View?) {
+                isExpanded = !isExpanded
+                if (isExpanded) {
+                    movieStory.maxLines = expandedMaxLines
+                    viewMore.text = "접기"
+                } else {
+                    movieStory.maxLines = originalMaxLines
+                    viewMore.text = "더보기"
+                }
+            }
+        })
+
+
     }
 }
