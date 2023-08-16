8aa8e281280de0a8107f7b7bae7dcac6f4701082
yspark2 <rotqjfrkffo@gmail.com>
2023-08-16 20:22:37 +0900

    update second commit

--- app/src/main/java/com/example/netcha/SignInActivity.kt
+++ app/src/main/java/com/example/netcha/SignInActivity.kt
@@ -19,7 +19,6 @@ class SignInActivity : AppCompatActivity() {
         }
 
         btnSignUp.setOnClickListener {
-
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/netcha/SignUpActivity.kt
+++ app/src/main/java/com/example/netcha/SignUpActivity.kt
@@ -2,10 +2,15 @@ package com.example.netcha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sigin_up)
+
+
     }
 }
