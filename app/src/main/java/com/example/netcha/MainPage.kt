e91f0d5c3725424f06f2c16e0761a522b00676e3
yspark2 <rotqjfrkffo@gmail.com>
2023-08-16 09:27:17 +0900

    update

--- app/src/main/java/com/example/netcha/MainPage.kt
+++ app/src/main/java/com/example/netcha/MainPage.kt
@@ -2,17 +2,33 @@ package com.example.netcha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
-//        val iv_profile1 = findViewById<ImageView>(R.id.iv_profile1)
-//        val iv_profile2 = findViewById<ImageView>(R.id.iv_profile2)
-//        val iv_profile3 = findViewById<ImageView>(R.id.iv_profile3)
-//        val iv_profile4 = findViewById<ImageView>(R.id.iv_profile4)
-//        val iv_profile5 = findViewById<ImageView>(R.id.iv_profile5)
+        val tvMovie1: TextView = findViewById(R.id.tv_movie1)
+        val btnSummary1: Button = findViewById(R.id.btn_summary1)
+
+        val originalMaxLines = tvMovie1.maxLines
+        val expandedMaxLines = Int.MAX_VALUE
+
+        btnSummary1.setOnClickListener(object : View.OnClickListener {
+            var isExpanded = false
+            override fun onClick(p0: View?) {
+                isExpanded = !isExpanded
+                if (isExpanded) {
+                    tvMovie1.maxLines = expandedMaxLines
+                    btnSummary1.text = "접기"
+                } else {
+                    tvMovie1.maxLines = originalMaxLines
+                    btnSummary1.text = "더보기"
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/example/netcha/Splash.kt
+++ app/src/main/java/com/example/netcha/Splash.kt
@@ -1,11 +1,13 @@
 package com.example.netcha
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
+// startActivity 아래 넣으시면 됩니다. (Activity 전환 애니메이션)
+// overridePendingTransition(들어 오는 부분,나가는 부분)
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +17,7 @@ class Splash : AppCompatActivity() {
 
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
-
+            overridePendingTransition(R.anim.slide_down_enter,R.anim.slide_down_exit)
             finish()
         }, 1500)
     }
