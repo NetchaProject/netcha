477dc159de4a6d6c4c590c6e30e82756cfe081b2
hamham1210 <thwn1012@gmail.com>
2023-08-16 23:22:05 +0900

    [DESIGN]detailpage- 사진 추가 및 ui 수정

--- app/src/main/java/com/example/netcha/MovieDetailPage2.kt
+++ app/src/main/java/com/example/netcha/MovieDetailPage2.kt
@@ -12,13 +12,13 @@ class MovieDetailPage2 : AppCompatActivity() {
         setContentView(R.layout.activity_movie_detail_page2)
 
         val image_sense = findViewById<ImageView>(R.id.famous_sense_img_hangover3)
-        var image_preview = findViewById<ImageView>(R.id.detailpage_image_preview1)
+        var image_preview = findViewById<ImageView>(R.id.detailpage_image_preview2)
         image_sense.setOnClickListener{
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://movie.daum.net/moviedb/main?movieId=111293"))
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://movie.daum.net/moviedb/main?movieId=66593"))
             startActivity(intent)
         }
         image_preview.setOnClickListener{
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=SEUyEpGHcsA&t=2s"))
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Ku5tWQvVhdA"))
             startActivity(intent)
         }
     }
