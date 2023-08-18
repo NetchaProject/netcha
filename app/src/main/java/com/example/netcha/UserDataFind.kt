62b43b1acde655954074431d55498fdee24c9490
youneeo <steamdacut@gmail.com>
2023-08-18 18:10:22 +0900

    Feature/UI update

--- app/src/main/java/com/example/netcha/UserDataFind.kt
+++ app/src/main/java/com/example/netcha/UserDataFind.kt
@@ -1,25 +1,27 @@
 package com.example.netcha
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 
 class UserDataFind : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_data_find)
 
-        val toolbar : Toolbar = findViewById(R.id.user_find_toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setDisplayShowHomeEnabled(true)
+        val btn = findViewById<ImageView>(R.id.backbutton_user_data_find)
+        btn.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit)
-        return true
+        val btn2 = findViewById<Button>(R.id.user_find_btn_login)
+        btn2.setOnClickListener {
+        }
     }
 }
